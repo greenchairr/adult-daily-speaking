ba3bdcf5628238bac6185b69e5f366b44f8b6db9
greenchairr <101319545+greenchairr@users.noreply.github.com>
2026-08-24 22:55:50 +0700

    Update units.js

--- units.js
+++ units.js
@@ -1,22 +1,22 @@
 window.allUnits = [
   { 
     id: 1, 
-    title: "Bài 1: Trò chuyện với con", 
+    title: "👶 Bài 1: Trò chuyện với con", 
     path: "units/unit1/data.js" 
   },
   { 
     id: 2, 
-    title: "Bài 2: Buổi tối trong gia đình", 
+    title: "🌙 Bài 2: Buổi tối trong gia đình", 
     path: "units/unit2/data.js" 
   },
   { 
     id: 3, 
-    title: "Bài 3: Con Chơi Đùa & An Toàn", 
+    title: "🧸 Bài 3: Con Chơi Đùa & An Toàn", 
     path: "units/unit3/data.js" 
   },
   { 
     id: 4, 
-    title: "Bài 4: Trò Chuyện Cùng Chồng", 
+    title: "💬 Bài 4: Trò Chuyện Cùng Chồng", 
     path: "units/unit4/data.js" 
   }
 ];
